3e67bc9b033d8f0826d85568bf38737f9f3cbcbe
kremerdesign <kremerdesign@gmail.com>
2015-01-06 09:58:00 -0800

    deployment testing

--- settings.py
+++ settings.py
@@ -250,7 +250,7 @@ INSTALLED_APPS = (
     "mezzanine.twitter",
     #"mezzanine.accounts",
     #"mezzanine.mobile",
-    # 'test_mezzanine',
+    'test_mezzanine',
 )
 
 # List of processors used by RequestContext to populate the context.
